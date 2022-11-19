31d5d2c623f95aae0dbfa66978ea6c7d7b8e311c
Katie <thompsonkatherine704@gmail.com>
2022-11-19 16:26:25 -0500

    Started homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,61 @@
+// Home Route includes existing blog posts if any have been posted; navigation links for the homepage and the dashboard; and the option to log in
+const router = require("express").Router();
+const { Blog, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+// Homepage Route to GET ALL Blog Posts
+router.get("/", async (req, res) => {
+  try {
+    // Get all Blog Posts and JOIN with user data
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      blogs,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Blog }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("dashboard", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
